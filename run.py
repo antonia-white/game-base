2380b6d47c58250f683092f502a8c71a73b6ada4
Toni <antonia.c.white1@gmail.com>
2022-06-27 13:34:41 +0000

    Add run.py file for basic flask app

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+from gamebase import app
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
+
