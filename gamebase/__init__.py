cef316745a5af7bcadd04cfef5b375f625f456e9
Toni <antonia.c.white1@gmail.com>
2022-06-29 14:17:31 +0000

    Update __init__.py file with heroku config

--- gamebase/__init__.py
+++ gamebase/__init__.py
@@ -1,15 +1,22 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 if os.path.exists("env.py"):
     import env # noqa
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE"] = os.environ.get("DATABASE_URL")
+
 
 
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 from gamebase import routes # noqa
