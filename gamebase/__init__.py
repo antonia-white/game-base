611677b66cb771203db6aa61d11bfee7ade332e1
Toni <antonia.c.white1@gmail.com>
2022-06-29 21:52:26 +0000

    Update __init__.py file

--- gamebase/__init__.py
+++ gamebase/__init__.py
@@ -1,26 +1,23 @@
 import os
 import re
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
-
+from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env # noqa
+    import env  # noqa
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-else:
-    uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri # heroku
-
-
+uri = os.environ.get("DB_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
 
-from gamebase import routes # noqa
+from gamebase import routes  # noqa
