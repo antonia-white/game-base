7983862480531c06d7af6b857f45356a205438d6
Toni <antonia.c.white1@gmail.com>
2022-07-04 16:10:36 +0000

    Add initialization for materialize collapsible elements in script.js

--- gamebase/static/js/script.js
+++ gamebase/static/js/script.js
@@ -5,7 +5,12 @@
 // select initialization
 let selects = document.querySelectorAll("select");
 M.FormSelect.init(selects);
+// collapsible initializataion
+let collapsibles = document.querySelectorAll(".collapsible");
+M.Collapsible.init(collapsibles);
 
-$(document).ready(function(){
+
+$(document).ready(function () {
     $('select').formSelect();
-  });
+    $(".collapsible").collapsible();
+});
