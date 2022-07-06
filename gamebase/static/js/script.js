e14e3b3e4cc15a49e0f7624b1f523b1f31ab6bf4
Toni <antonia.c.white1@gmail.com>
2022-07-06 17:41:57 +0000

    Initization of materialize navbar in script.js

--- gamebase/static/js/script.js
+++ gamebase/static/js/script.js
@@ -2,15 +2,16 @@
     jQuery for MaterializeCSS initialization
 */
 
-// select initialization
-let selects = document.querySelectorAll("select");
-M.FormSelect.init(selects);
-// collapsible initializataion
-let collapsibles = document.querySelectorAll(".collapsible");
-M.Collapsible.init(collapsibles);
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
 
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 
-$(document).ready(function () {
-    $('select').formSelect();
-    $(".collapsible").collapsible();
-});
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+})
