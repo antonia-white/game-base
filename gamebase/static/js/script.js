61bc9cf34617298898b99f3489fbff39d646925f
Toni <antonia.c.white1@gmail.com>
2022-07-15 14:56:19 +0000

    Add function to alert user to input genre with a color change

--- gamebase/static/js/script.js
+++ gamebase/static/js/script.js
@@ -35,3 +35,27 @@ document.addEventListener("DOMContentLoaded", function () {
     let modals = document.querySelectorAll(".modal");
     M.Modal.init(modals);
 })
+
+document.querySelector(document).ready(function() {
+    document.querySelector("select").material_select();
+
+    // for HTML5 "required" attribute
+    document.querySelector("select[required]").css({
+        display: 'inline',
+        position: 'absolute',
+        float: 'left',
+        padding: 0,
+        margin: 0,
+        border: '1px solid rgba(255,255,255,0)',
+        height: 0, 
+        width: 0,
+        top: '2em',
+        left: '3em',
+        opacity: 0,
+        pointerEvents: 'none'
+      });
+})
+
+function requiredGenre() {
+    document.getElementById('required-genre').style.cssText = 'color: #F44336;'
+}
