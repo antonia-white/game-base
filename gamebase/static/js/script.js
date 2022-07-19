f72bff0a725812b885297e9eca3f1e8eba523598
Toni <antonia.c.white1@gmail.com>
2022-07-19 16:19:34 +0000

    Add functionality to prevent keyboard input for date input in script.js

--- gamebase/static/js/script.js
+++ gamebase/static/js/script.js
@@ -37,41 +37,11 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    validateMaterializeSelect();
-    function validateMaterializeSelect() {
-        let classValid = "border-bottom: 1px solid #4caf50; box-shadow: 0 1px 0 0 #4caf50;";
-        let classInvalid = "border-bottom: 1px solid #f44336; box-shadow: 0 1px 0 0 #f44336;";
-        let selectValidate = document.querySelector("select.validate");
-        let selectWrapperInput = document.querySelector(".select-wrapper input.select-dropdown");
-        if (selectValidate.hasAttribute("required")) {
-            selectValidate.style.cssText = "height: 0; padding: 0; width: 0; position: absolute;";
-        }
-        selectWrapperInput.addEventListener("focusin", (e) => {
-            e.target.parentNode.addEventListener("change", () => {
-                ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
-                for (let i = 0; i < ulSelectOptions.length; i++) {
-                    if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled") {
-                        e.target.style.cssText = classValid;
-                    }
-                }
-            });
-        });
-        selectWrapperInput.addEventListener("click", (e) => {
-            ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
-            for (let i = 0; i < ulSelectOptions.length; i++) {
-                if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled" && ulSelectOptions[i].style.backgroundColor == "rgba(0, 0, 0, 0.03)") {
-                    e.target.style.cssText = classValid;
-                } else {
-                    e.target.addEventListener("focusout", () => {
-                        if (e.target.parentNode.childNodes[3].hasAttribute("required")) {
-                            if (e.target.style.borderBottom != "1px solid rgb(76, 175, 80)") {
-                                e.target.style.cssText = classInvalid;
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    }
-});
+function requiredGenre() {
+    document.getElementById('required-genre').style.cssText = 'color: #F44336;';
+}
+
+// Stop keyboard input on date field
+function preventKeyboardInput(event) {
+    event.preventDefault();
+  }
