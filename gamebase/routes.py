c8f66e924b274386b12ab7921f9b2d1a56cca088
Toni <antonia.c.white1@gmail.com>
2022-07-20 08:45:34 +0000

    Add error 500 route to routes.py file

--- gamebase/routes.py
+++ gamebase/routes.py
@@ -349,3 +349,9 @@ def logout():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html")
+
+
+# 500 error handling
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html")
